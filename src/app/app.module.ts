a20325c3608e9db9f8e700d4caeedc35d6d35aef
bilal <mail2muhammedbilal@gmail.com>
2023-09-29 02:31:51 +0530

    feat: vendor list page update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VendorListPageComponent } from './vendor-list-page/vendor-list-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VendorListPageComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/vendor-list-page/vendor-list-page.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vendor-list-page',
+  templateUrl: './vendor-list-page.component.html',
+  styleUrls: ['./vendor-list-page.component.css'],
+})
+export class VendorListPageComponent implements OnInit {
+  cards = [
+    {
+      label: 'total vendors',
+      count: 7531,
+    },
+    {
+      label: 'pending headers',
+      count: 42,
+    },
+    {
+      label: 'total suppliers',
+      count: 72,
+    },
+    {
+      label: 'onboarding',
+      count: 17,
+    },
+    {
+      label: 'modification draft',
+      count: 42,
+    },
+    {
+      label: 'new vendors',
+      count: 285,
+      increment: 6,
+    },
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
